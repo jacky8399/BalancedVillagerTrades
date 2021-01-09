16cf5bb309e32d5f57817dfde0754237297cedfa
jacky8399 <jacky8399email@gmail.com>
2021-01-09 20:35:42 +0800

    Initial commit

---
+++ src/main/java/com/jacky8399/balancedvillagertrades/BalancedVillagerTrades.java
@@ -0,0 +1,17 @@
+package com.jacky8399.balancedvillagertrades;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BalancedVillagerTrades extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
